868b854d8997ff491bdae6640c93b8738e22a4f5
Jordan Imp <49874843+jsi238@users.noreply.github.com>
2024-10-14 23:40:29 -0400

    Added flake8 file, black reformatted

--- Django/mysite/polls/tests.py
+++ Django/mysite/polls/tests.py
@@ -7,34 +7,37 @@ from django.urls import reverse
 
 from .models import Question
 
+
 # Create your tests here.
 class QuestionModelTest(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
-    
+
     def test_was_published_recently_with_old_question(self):
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
-    
+
     def test_was_published_recently_with_recent_question(self):
         time = timezone.now()
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
-    
+
+
 class QuestionIndexViewTest(TestCase):
     def test_no_questions(self):
         response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
-            
+
     def test_past_question(self):
         """
         Questions with a pub_date in the past are displayed on the
@@ -81,7 +84,7 @@ class QuestionIndexViewTest(TestCase):
             response.context["latest_question_list"],
             [question2, question1],
         )
-    
+
     def test_future_question(self):
         """
         The detail view of a question with a pub_date in the future
--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -12,14 +12,18 @@ from django.utils import timezone
 
 from .models import Choice, Question
 
+
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-    
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
@@ -27,9 +31,11 @@ class DetailView(generic.DetailView):
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
