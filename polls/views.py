9a34f64590b5589880df055c7b11050c40668074
Jordan Imp <49874843+jsi238@users.noreply.github.com>
2024-10-15 01:23:14 -0400

    Stupid flake8

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
